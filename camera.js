b32d42dafb785d19b11f86705b95ab62bd0afcff
lunears <adam.dahl1@gmail.com>
2014-07-05 12:00:25 -0700

    Update

--- camera.js
+++ camera.js
@@ -1,1 +1,9 @@
-var isstarted = 1
+ var errorCallback = function(e) {
+    console.log('ERROR: Cannot use camera D:!', e);
+  };
+  navigator.getUserMedia({video: true, audio: true}, function(localMediaStream) {
+    var video = document.querySelector('video');
+    video.src = window.URL.createObjectURL(localMediaStream);
+    video.onloadedmetadata = function(e) {
+    };
+  }, errorCallback);
