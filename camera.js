4f00334e36b40468b680b60b84f907a34cd9b760
lunears <adam.dahl1@gmail.com>
2014-07-05 12:04:57 -0700

    Revert "Update"
    This reverts commit b32d42dafb785d19b11f86705b95ab62bd0afcff.

--- camera.js
+++ camera.js
@@ -1,9 +1,1 @@
- var errorCallback = function(e) {
-    console.log('ERROR: Cannot use camera D:!', e);
-  };
-  navigator.getUserMedia({video: true, audio: true}, function(localMediaStream) {
-    var video = document.querySelector('video');
-    video.src = window.URL.createObjectURL(localMediaStream);
-    video.onloadedmetadata = function(e) {
-    };
-  }, errorCallback);
+var isstarted = 1
