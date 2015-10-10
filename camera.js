f8e921c25b4c3dd1485cf0d6074866ed531251fb
lunears <Adam@Adams-Mac-mini.local>
2015-10-10 15:52:23 -0700

    Added Comments and Removed Unnecessary Variables
    curframe is already an integer.

--- camera.js
+++ camera.js
@@ -1,5 +1,5 @@
 var curframe = 1;
-var doit = "doit";
+//Variables to use later
 window.addEventListener("DOMContentLoaded", function () {
 		// Grab elements, create settings, etc.
 		var canvas = document.getElementById("canvas"),
@@ -14,6 +14,7 @@ window.addEventListener("DOMContentLoaded", function () {
 				navigator.getUserMedia(videoObj, function (stream) {
 						video.src = stream;
 						video.play();
+						// Show what is on camera.
 				}, errBack);
 		} else if (navigator.webkitGetUserMedia) { // WebKit-prefixed
 				navigator.webkitGetUserMedia(videoObj, function (stream) {
@@ -24,16 +25,25 @@ window.addEventListener("DOMContentLoaded", function () {
 		// Trigger photo take
 		document.getElementById("snap").addEventListener("click", function () {
 				context.drawImage(video, 0, 0, 320, 240);
+				// Make grab image from video.
 				eval('var frame' + curframe + ' = canvas.toDataURL("image/png")');
+				// Get Image URL
 				eval('var frame = '+"frame"+curframe+'');
+				// Set current frame based on variable that changes
 				document.getElementById("frame"+curframe).innerHTML = '<img width="160" height="120" src="'+frame+'"/>';
-				curframe = parseInt(curframe);
+				// Show image taken on page, based on current frame.
 				console.log("cur1: "+curframe);
+				// Debug
 				curframe = curframe + 1;
+				// Make current frame the next frame.
 				console.log("cur2: "+curframe)
+				// Debug
 				if (curframe >= 5){
+				// if the current frame is now 5 or higher (needs to check for higher becuase the equal was changing the variable)
 				console.log("curif: "+curframe);
+				// Debug
 				curframe = 1;
+				// Set the current frame to 1 so preview can be updated accordingly.
 			}
 
 		});
