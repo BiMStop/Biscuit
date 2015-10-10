d7d6ac14307be6e779f12cac77993ddac237b77f
Charlie Lee <charlielee735@gmail.com>
2015-10-10 12:20:09 +0100

    Improved navigation bar
    Based on a list rather than a table. Styles moved to CSS.

--- camera.js
+++ camera.js
@@ -1,4 +1,4 @@
-// Put event listeners into place
+var curframe = 1;
 window.addEventListener("DOMContentLoaded", function () {
 		// Grab elements, create settings, etc.
 		var canvas = document.getElementById("canvas"),
@@ -23,5 +23,12 @@ window.addEventListener("DOMContentLoaded", function () {
 		// Trigger photo take
 		document.getElementById("snap").addEventListener("click", function () {
 				context.drawImage(video, 0, 0, 320, 240);
+				eval('var frame' + curframe + ' = canvas.toDataURL("image/png")');
+				eval('var frame = '+"frame"+curframe+'');
+				console.log(frame);
+				console.log(frame1);
+				document.getElementById("frame"+curframe).innerHTML = '<img width="160" height="120" src="'+frame+'"/>';
+				curframe = parseInt(curframe);
+				curframe = curframe + 1;
 		});
 }, false);
