e15ec962b6da575eb0e09329acf4c1dff4d0a9ea
lunears <Adam@Adams-Mac-mini.local>
2015-10-13 13:54:31 -0700

    Frames array reset on close.

--- main.js
+++ main.js
@@ -34,9 +34,7 @@ app.on('ready', function() {
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
+    framearr = '';
     mainWindow = null;
   });
 });
