2e9e8668dfa61ab568c7551d0c8631bd7d68757e
lunears <Adam@Adams-Mac-mini.local>
2015-10-30 20:37:53 -0700

    More more.

--- pagecontroller.js
+++ pagecontroller.js
@@ -4,17 +4,27 @@ $(document).ready(function() {
   $("a").click(function(event) {
     event.preventDefault();
     linkLocation = this.href;
+    console.log(this.href);
     $("div.page").fadeOut(100, redirectPage);
   });
 
-  function redirectPage() {
+  function redirectPage(link) {
+    console.log('12');
     var xhttp = new XMLHttpRequest();
+    console.log('14');
     xhttp.onreadystatechange = function() {
+      console.log('16');
       if (xhttp.readyState == 4 && xhttp.status == 200) {
-        document.querySelector("#page").innerHTML = xhttp.responseText;
+        console.log('18');
+        document.getElementById("page").innerHTML = xhttp.responseText;
+        console.log('20');
       }
+      console.log('22');
     }
-    xhttp.open("GET", linkLocation, true);
+    console.log('24');
+    xhttp.open("GET", link, true);
+    console.log('26');
     xhttp.send();
+    console.log('28');
   }
 });
