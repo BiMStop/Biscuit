66d19f30ab988a1195ab0ca734fe97390d9c903a
lunears <Adam@Adams-Mac-mini.local>
2015-10-31 13:06:49 -0700

    Fixed

--- pagecontroller.js
+++ pagecontroller.js
@@ -1,28 +1,38 @@
 $(document).ready(function() {
-  $("body").fadeIn(100);
+  $("body").fadeIn(300);
 });
-function loadcamera(){
-  $("iframe").fadeOut(100);
-  $('#iframe1').attr('src','camera.html' );
-  $("iframe").fadeIn(100);
+
+function loadcamera() {
+  $("iframe").fadeOut(300);
+  $('#iframe').attr('src', 'camera.html').load(function() {
+    $("iframe").fadeIn(300);
+  });
 }
-function loadhome(){
-  $("iframe").fadeOut(100);
-  $('#iframe1').attr('src','indexc.html' );
-  $("iframe").fadeIn(100);
+
+function loadhome() {
+  $("iframe").fadeOut(300);
+  $('#iframe').attr('src', 'indexc.html').load(function() {
+    $("iframe").fadeIn(300);
+  });
 }
-function loadaudio(){
-  $("iframe").fadeOut(100);
-  $('#iframe1').attr('src','audio.html' );
-  $("iframe").fadeIn(100);
+
+function loadaudio() {
+  $("iframe").fadeOut(300);
+  $('#iframe').attr('src', 'audio.html').load(function() {
+    $("iframe").fadeIn(300);
+  });
 }
-function loadwrite(){
-  $("iframe").fadeOut(100);
-  $('#iframe1').attr('src','write.html' );
-  $("iframe").fadeIn(100);
+
+function loadwrite() {
+  $("iframe").fadeOut(300);
+  $('#iframe').attr('src', 'write.html').load(function() {
+    $("iframe").fadeIn(300);
+  });
 }
-function loadabout(){
-  $("iframe").fadeOut(100);
-  $('#iframe1').attr('src','camera.html' );
-  $("iframe").fadeIn(100);
+
+function loadabout() {
+  $("iframe").fadeOut(300);
+  $('#iframe').attr('src', 'camera.html').load(function() {
+    $("iframe").fadeIn(300);
+  });
 }
