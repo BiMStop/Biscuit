256e9bfab4c4d2bf41317cea74a764d66b7b0fd3
lunears <Adam@Adams-Mac-mini.local>
2015-10-30 20:51:16 -0700

    Dear me.

--- pagecontroller.js
+++ pagecontroller.js
@@ -8,23 +8,12 @@ $(document).ready(function() {
     $("div.page").fadeOut(100, redirectPage);
   });
 
-  function redirectPage(link) {
-    console.log('12');
-    var xhttp = new XMLHttpRequest();
-    console.log('14');
-    xhttp.onreadystatechange = function() {
-      console.log('16');
-      if (xhttp.readyState == 4 && xhttp.status == 200) {
-        console.log('18');
-        document.getElementById("page").innerHTML = xhttp.responseText;
-        console.log('20');
-      }
-      console.log('22');
-    }
-    console.log('24');
-    xhttp.open("GET", link, true);
-    console.log('26');
-    xhttp.send();
-    console.log('28');
+  function redirectPage() {
+    $.ajax({
+      url: "camera.html",
+      context: document.body
+    }).done(function() {
+      $( this ).addClass( "done" );
+    });
   }
 });
