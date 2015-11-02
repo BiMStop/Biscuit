0cc68764988dcf4ce833df1c7cd8494b10ca4977
lunears <Adam@Adams-Mac-mini.local>
2015-11-01 17:47:16 -0700

    Fixed camera (after I broke it)

--- camera.js
+++ camera.js
@@ -155,6 +155,7 @@ window.addEventListener("DOMContentLoaded", function() {
     document.querySelector(".frame").height = 0;
     framearr.splice(selframe);
   });
+  // Using getElementById becuase querySelector didn't want to work.
   document.getElementById("btn-export").addEventListener("click", function() {
     mkdir(tmp+'Biscuit');
     var frexst = document.querySelector("#exframerate").value
