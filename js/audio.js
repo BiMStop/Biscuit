11adb6cf5ff718ecad788407a551c47998080974
lunears <Adam@Adams-Mac-mini.local>
2015-11-05 19:21:50 -0700

    Re-organization

--- js/audio.js
+++ js/audio.js
@@ -1,7 +1,7 @@
 (function(window) {
 
-  var WORKER_PATH = 'lib/js/recorderWorker.js';
-  var encoderWorker = new Worker('lib/js/mp3Worker.js');
+  var WORKER_PATH = 'js/recorderWorker.js';
+  var encoderWorker = new Worker('js/mp3Worker.js');
 
   var Recorder = function(source, cfg) {
     var config = cfg || {};
